4d14a77c5d5b61e3665b05bffae89d956f810457
Dr. Dillinger <peter.dillinger@srh.de>
2023-10-31 10:51:46 +0100

    Initial commit

---
+++ src/Sternenausgabe.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Sternenausgabe {
+    public static void main(String[] args) {
+        Scanner eingabe = new Scanner(System.in);
+        System.out.print("Anzahl Zeilen: ");
+        int anzahlDerZeilen = eingabe.nextInt();
+
+        // TODO
+    }
+}
